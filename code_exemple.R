62dbedfb0b2d838f0889eb246df7d9daa5e7eeb9
rflicoteaux <remi.flicoteaux@aphp.fr>
2021-11-15 14:53:41 +0100

    maj

--- code_exemple.R
+++ code_exemple.R
@@ -1,3 +1,9 @@
+Sys.setenv(http_proxy="http://proxym-inter.aphp.fr:8080")
+Sys.setenv(https_proxy="http://proxym-inter.aphp.fr:8080")
+
+install.packages("tidyverse")
+install.packages("remotes")
+remotes::install_github('guillaumepressiat/pmeasyr')
 
 ## Exemple de création d'un objet avec R
 x <- 1:10
@@ -15,6 +21,75 @@ typeof(x)
 
 # Trouver la taille d'un vecteur
 length(x)
+
+#Créer un dataframe
+x <- data.frame(x= 1:10, y= 21:30)
+
+#Type d'objets
+str(x)
+class(x)
+typeof(x)
+
+#Nom des colonnes
+names(x)
+
+#Accéder aux données élémentaires
+line_number = 1
+columun_number = 2
+x[line_number , columun_number]
+
+#Noms des lignes et des colonnes
+names(x)
+row.names(x)
+
+#Accéder aux données élémentaires par les noms des dimensions
+line_name = "1"
+columun_name = "x"
+x[line_name , columun_name]
+
+#Sélectionner une colonne 
+x$x
+
+# Ajouter 5 à la première colonne
+x$x + 5
+
+
+#Utiliser le pipe
+`%>%` <- magrittr::`%>%`
+
+x + 5 -3
+
+x %>% + 5 %>% -3
+
+
+
+#Dézippage des RUM
+pmeasyr::adezip(finess = 750712184, 
+                annee = 2021, 
+                mois = 8, 
+                path = 'C:/Users/3056269/Documents/data/mco', 
+                liste = c("rss"), 
+                type = "in")
+
+
+#Import des RSS
+pmeasyr::irum(finess = 750712184, 
+              annee = 2021, 
+              mois = 8, 
+              path = 'C:/Users/3056269/Documents/data/mco', 
+              typi = 4, 
+              tolower_names = TRUE ) -> rum21
+typeof(rum21)
+names(rum21)
+
+library(dplyr)
+
+rum21$rum %>% dplyr::filter(dp == "I10")
+
+rum21$rum %>% filter(dp == "I10") %>%
+  mutate(moissor = format(d8soue,"%m")  )%>%
+  select(nas,moissor)
+
 
 #Nobmre de séjours par mois dont le diagnostic princial est I10
 rum21$rum %>% dplyr::filter(dp == "I10") %>%
@@ -36,3 +111,24 @@ rum21$rum %>% dplyr::filter(dp == "I10") %>%
   mutate(moissor = format(dtsor,"%m")  ) %>%
   group_by(moissor) %>%
   summarise(nb = length(unique(nas)))
+
+#Rechercher I10 dans les DAS
+rum21$das %>% dplyr::filter(das == "I10") -> das_i10
+
+rum21$rum %>% dplyr::filter(dp == "I10" | nas %in% das_i10$nas ) %>%
+  left_join(.,dates_sejours) %>%
+  mutate(moissor = format(dtsor,"%m")  ) %>%
+  group_by(moissor) %>%
+  summarise(nb = length(unique(nas)))
+
+
+rum21$rum %>% dplyr::filter(dp == "I10" | nas %in% das_i10$nas, 
+                            substr(cdresi,1,2) == "75" ) %>%
+  left_join(.,dates_sejours) %>%
+  mutate(moissor = format(dtsor,"%m")  ) %>%
+  group_by(moissor) %>%
+  summarise(nb = length(unique(nas)))
+
+
+#Charger Référime
+devtools::install_github('https://github.com/NamikTaright/referime.git')
